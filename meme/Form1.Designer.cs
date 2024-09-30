a289cc55e3ec2ab163701c4305d152ca592fcd83
WIN-Make <141756991+WIN-Make@users.noreply.github.com>
2024-09-30 10:50:05 +0300

    Add project files.

---
+++ meme/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace meme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(339, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(427, 337);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Memesomeware";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ meme/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace meme
+{
+    public partial class Form1 : Form
+    {
+        private int remainingTime = 60;
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("You executed Meme.exe that will encrypt your files and if you do not enter the corect password in 1 minute all of your files will be deleated, good luck");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "S{h96KrFcb07/6vs{9b4]37b1RxZ0l`S94=OZAzK>£*£;HR9£:" || textBox1.Text == "7>6i5=>8_~yfe8ug.!*#[nnwbaa5t2gh*n" || textBox1.Text == "PxQ710M89McmHZY2fA0mzznU8fTeOVkS4AySHxyst2veF72vFD")
+            {
+                if (remainingTime > 0)
+                    MessageBox.Show("You got the corect password , Congrats");
+                    timer1.Stop();
+                    MessageBox.Show("All of your files will not be deleaed , Yay");
+                    Application.Exit();
+
+            }
+            else
+            {
+                MessageBox.Show("You got the wrong password");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (remainingTime > 0)
+            {
+                remainingTime--;
+                label1.Text = "Time : " + remainingTime.ToString() + " seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("All of your files are deleated now , sorry");
+                MessageBox.Show("Bye!");
+
+                Application.Exit();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Time : " + remainingTime.ToString() + " seconds";
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+    }
+}
